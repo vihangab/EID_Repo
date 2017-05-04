d704e3cadeb6c900188a871c6ade9f97329a40c7
root <root@raspberrypi>
2017-05-03 23:03:44 -0600

    Working code with pub sub, date time

--- connect_device_package/awsiotpub.py
+++ connect_device_package/awsiotpub.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 
-# this source is part of my Hackster.io project:  https://www.hackster.io/mariocannistra/radio-astronomy-with-rtl-sdr-raspberrypi-and-amazon-aws-iot-45b617
-
 # use this program to test the AWS IoT certificates received by the author
 # to participate to the spectrogram sharing initiative on AWS cloud
 
@@ -9,6 +7,10 @@
 # to test this program you have to run first its companion awsiotsub.py
 # that will subscribe and show all the messages sent by this program
 
+
+import sys
+import time
+import Adafruit_DHT
 import paho.mqtt.client as paho
 import os
 import socket
@@ -19,6 +21,9 @@ from time import sleep
 from random import uniform
 
 connflag = False
+sensor = 22  #DHT22 sensor
+pin = 4      #GPIO Pin 4 (Pin 7)
+
 
 def on_connect(client, userdata, flags, rc):
     global connflag
@@ -44,6 +49,7 @@ mqttc.on_message = on_message
 #certPath = "VB_Rpi3.cert.pem"
 #keyPath = "VB_Rpi3.private.key"
 
+
 awshost = "a2vp65ivl2lw2v.iot.us-west-2.amazonaws.com"
 awsport = 8883
 clientId = "MyRPi3"
@@ -62,11 +68,14 @@ mqttc.loop_start()
 while 1==1:
     sleep(0.5)
     if connflag == True:
-        tempreading = uniform(15.0,25.0)
-        timestamp = datetime.datetime.now().strftime("%Y%m%d-%H:%M:%S")
-        result_json={'UserID':1005,'Username':"Virag", 'Temp':tempreading}
-        mqttc.publish("temperature",json.dumps(result_json),0)
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+        temperature = round(temperature,2)
+        humidity = round(humidity,2)
+        date = datetime.datetime.now().strftime("%Y%m%d")
+        time = datetime.datetime.now().strftime("%H:%M")
+	result_json = {'Date':"date",'Time':"time",'Temperature':temperature,'Humidity':humidity}
+        mqttc.publish("temperature", json.dumps(result_json),0)
         #mqttc.publish("aws/things/VB_Rpi3/Temperature", tempreading, qos=1)
-        print("msg sent: temperature " + "%.2f" % tempreading )
+        print("msg sent: temperature " + "%.2f" % temperature )
     else:
         print("waiting for connection...")
